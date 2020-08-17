110406e4a550be831b3c72343aac4d27d5a512df
Dominic Hupp <dhupp2020@gmail.com>
2020-08-17 08:41:17 -0400

    Add files via upload

---
+++ PrelabTwo/src/Hupp_PrelabTwo_PartOne.cpp
@@ -0,0 +1,13 @@
+//Prelab 2 - Dominic Hupp - Part One
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+	cout << "Hi, my name is Hal!\n" << "What would you like me to do? ";
+	string user_input;
+	getline(cin, user_input);
+	cout << "I am sorry, I cannot do that.";
+}
