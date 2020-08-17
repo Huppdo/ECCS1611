3f367a2d5e7804a8eb62ee1b339b30ce1db7dbe5
Dominic Hupp <dhupp2020@gmail.com>
2020-08-17 19:32:12 -0400

    Submitted Lab 2

---
+++ LabTwo/src/Hupp_Lab2_Part2.cpp
@@ -0,0 +1,14 @@
+//Dominic Hupp - ECCS 1611 Lab Two - Part Two
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for (int i = 0; i<=10; i++)
+	{
+		sum += i;
+	}
+	cout << sum;
+}
---
+++ LabTwo/src/Hupp_Lab2_Part3.cpp
@@ -0,0 +1,14 @@
+//Dominic Hupp - ECCS 1611 Lab Two - Part Three
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int product = 1;
+	for (int i = 1; i <= 10; i++)
+	{
+		product = product * i;
+	}
+	cout << product;
+}
