ca5acfe9f22ce9574213538678ae9fa3079dc89a
Dominic Hupp <dhupp2020@gmail.com>
2020-08-21 08:42:10 -0400

    Uploaded new content for organizational purposes

---
+++ Labs/PrelabTwo/src/Hupp_PrelabTwo_PartTwo.cpp
@@ -0,0 +1,14 @@
+// Prelab 2 - Dominic Hupp - Part 2
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int sum = 0;
+	for (int i = 1; i <= 5; i++)
+	{
+		sum += i;
+	}
+	cout << sum;
+}
