07705539913d440936fb02360414b5fdc25969dc
Dominic Hupp <dhupp2020@gmail.com>
2020-08-25 11:42:09 -0400

    Flipped variable declarations for problem solving

--- Labs/PrelabThree/src/Hupp_Prelab02_Source.cpp
+++ Labs/PrelabThree/src/Hupp_Prelab02_Source.cpp
@@ -9,7 +9,7 @@ int main() {
 	cout.precision(2); //Displays to one place after the decimal
 
 	int fenceLength = 0; //will always be a multiple of 15!
-	
+		
 	//constants
 	const int LENGTH_PER_RAIL = 15, RAILS_PER_SECTION = 4;
 	const double COST_PER_POST = 16.00, COST_PER_RAIL = 20.00;
@@ -23,8 +23,8 @@ int main() {
 	cin >> fenceLength;
 
 	//calculate number of rails and posts
-	rails = (fenceLength / LENGTH_PER_RAIL) + 1;
-	posts = (rails - 1) * RAILS_PER_SECTION;
+	posts = (fenceLength / LENGTH_PER_RAIL) + 1;
+	rails = (posts - 1) * RAILS_PER_SECTION;
 
 	//calculate final cost
 	finalCost = (rails * COST_PER_RAIL) + (posts * COST_PER_POST);
