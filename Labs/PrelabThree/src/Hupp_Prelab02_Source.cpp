24fe7d2c5542c7fc766291f10dad9f0ea318fbe5
Dominic Hupp <dhupp2020@gmail.com>
2020-08-25 11:36:47 -0400

    Edited source and predetermined values
    This source code is no longer  consistent with the test cases found in AllAttempts

--- Labs/PrelabThree/src/Hupp_Prelab02_Source.cpp
+++ Labs/PrelabThree/src/Hupp_Prelab02_Source.cpp
@@ -8,30 +8,23 @@ int main() {
 	cout.setf(ios::showpoint); //Displays zeros for floating point representations, even for whole numbers
 	cout.precision(2); //Displays to one place after the decimal
 
-	int fenceLength = 0; //will always be a multiple of 10!
+	int fenceLength = 0; //will always be a multiple of 15!
 	
 	//constants
-	const int LENGTH_PER_RAIL = 10;
-	const int RAILS_PER_SECTION = 3;
-	const double COST_PER_POST = 10.98;
-	const double COST_PER_RAIL = 9.97;
+	const int LENGTH_PER_RAIL = 15, RAILS_PER_SECTION = 4;
+	const double COST_PER_POST = 16.00, COST_PER_RAIL = 20.00;
 
 	//needs computed
-	int sections = 0;
-	int rails = 0;
-	int posts = 0;
+	int rails = 0, posts = 0;
 	double finalCost = 0;
 
 	//get fence length
-	cout << "Please input your desired fence length (as a multiple of 10):  ";
+	cout << "Please input your desired fence length (as a multiple of 15):  ";
 	cin >> fenceLength;
-	
-	//calculate number of sections based on rail length
-	sections = fenceLength / LENGTH_PER_RAIL;
 
 	//calculate number of rails and posts
-	rails = sections * RAILS_PER_SECTION;
-	posts = sections + 1;
+	rails = (fenceLength / LENGTH_PER_RAIL) + 1;
+	posts = (rails - 1) * RAILS_PER_SECTION;
 
 	//calculate final cost
 	finalCost = (rails * COST_PER_RAIL) + (posts * COST_PER_POST);
